c62803a54406ca7ce8ce948bb3fc54ff7f29e6e9
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-04-02 21:25:24 +0300

    Vue test

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 <div class="container">
     <div class="canvas">
         <svg class="chart" width="500" height="500" viewBox="0 0 50 50">
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
-            <circle class="unit" r="15.9" cx="50%" cy="50%"></circle>
+            <circle v-for="(c,k) in [1,2,3,4,5,6,7]" 
+                   :r="radius" 
+                   :cx="cx" 
+                   :cy="cy" 
+                   :class="cssclass" 
+                   :key="k"></circle>
+           
         </svg>
     </div>  
         <div class="legend">
@@ -25,3 +25,18 @@
     </div>
 </div>
 </template>
+<script>
+export default {
+   data () {
+       return {
+           radius: 10,
+           cx: "50%",
+           cy: "50%",
+           cssclass:"unit"
+                  
+       }   
+   }
+
+}
+
+</script>
